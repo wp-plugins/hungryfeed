6ae25c5c6c195937e952a1a2ef75eb3c934dd1b2
verysimple <verysimple@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-22 04:21:17 +0000

    Update to 1.2.0.  Add link_item_title and date_format params
    git-svn-id: https://plugins.svn.wordpress.org/hungryfeed/trunk@265678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hungryfeed/hungryfeed.php
+++ hungryfeed/hungryfeed.php
@@ -3,18 +3,20 @@
 Plugin Name: HungryFEED
 Plugin URI: http://verysimple.com/products/hungryfeed/
 Description: HungryFEED displays RSS feeds on a page or post using Shortcodes.	Respect!
-Version: 1.1.0
+Version: 1.2.0
 Author: VerySimple
 Author URI: http://verysimple.com/
 License: GPL2
 */
 
-define('HUNGRYFEED_VERSION','1.1.0');
+define('HUNGRYFEED_VERSION','1.2.0');
 define('HUNGRYFEED_DEFAULT_CACHE_DURATION',3600);
 define('HUNGRYFEED_DEFAULT_CSS',"h3.hungryfeed_feed_title {}\np.hungryfeed_feed_description {}\ndiv.hungryfeed_items {}\ndiv.hungryfeed_item {margin-bottom: 10px;}\ndiv.hungryfeed_item_title {font-weight: bold;}\ndiv.hungryfeed_item_description {}\ndiv.hungryfeed_item_author {}\ndiv.hungryfeed_item_date {}");
 define('HUNGRYFEED_DEFAULT_CACHE_LOCATION',ABSPATH . 'wp-content/cache');
 define('HUNGRYFEED_DEFAULT_FEED_FIELDS','title,description');
 define('HUNGRYFEED_DEFAULT_ITEM_FIELDS','title,description,author,date');
+define('HUNGRYFEED_DEFAULT_LINK_ITEM_TITLE',1);
+define('HUNGRYFEED_DEFAULT_DATE_FORMAT','F j, Y, g:i a');
 
 /**
  * import supporting libraries
@@ -40,9 +42,11 @@ function hungryfeed_display_rss($params)
 	$xml_dump = hungryfeed_val($params,'xml_dump','0');
 	$decode_url = hungryfeed_val($params,'decode_url','1');
 	$max_items = hungryfeed_val($params,'max_items',0);
+	$date_format = hungryfeed_val($params,'date_format',HUNGRYFEED_DEFAULT_DATE_FORMAT);
 	
 	$feed_fields = explode(",", hungryfeed_val($params,'feed_fields',HUNGRYFEED_DEFAULT_FEED_FIELDS));
 	$item_fields = explode(",", hungryfeed_val($params,'item_fields',HUNGRYFEED_DEFAULT_ITEM_FIELDS));
+	$link_item_title = hungryfeed_val($params,'link_item_title',HUNGRYFEED_DEFAULT_LINK_ITEM_TITLE);
 	
 	// fix weirdness in the url due to the wordpress visual editor
 	if ($decode_url) $url = html_entity_decode($url);
@@ -112,10 +116,16 @@ function hungryfeed_display_rss($params)
 		if ($max_items > 0 && $counter > $max_items) break;
 		
 		echo "<div class=\"hungryfeed_item\">\n";
-			if (in_array("title",$item_fields)) echo '<div class="hungryfeed_item_title"><a href="' . $item->get_permalink() . '">' . $item->get_title() . "</a></div>\n";
-			if (in_array("description",$item_fields)) echo '<div class="hungryfeed_item_description">' . $item->get_description() . "</div>\n";
-			if (in_array("author",$item_fields)) echo '<div class="hungryfeed_item_author">' . $item->get_author()->name . "</div>\n";
-			if (in_array("date",$item_fields)) echo '<div class="hungryfeed_item_date">' . $item->get_date($date_format) .  "</div>\n";
+			if (in_array("title",$item_fields)) 
+				echo $link_item_title 
+					? '<div class="hungryfeed_item_title"><a href="' . $item->get_permalink() . '">' . $item->get_title() . "</a></div>\n"
+					: '<div class="hungryfeed_item_title">' . $item->get_title() . '</div>';
+			if (in_array("description",$item_fields)) 
+				echo '<div class="hungryfeed_item_description">' . $item->get_description() . "</div>\n";
+			if ($item->get_author()->name && in_array("author",$item_fields)) 
+				echo '<div class="hungryfeed_item_author">Author: ' . $item->get_author()->name . "</div>\n";
+			if ($item->get_date() && in_array("date",$item_fields)) 
+				echo '<div class="hungryfeed_item_date">Posted: ' . $item->get_date($date_format) . "</div>\n";
 		echo "</div>\n";
 	}
 	
--- readme.txt
+++ readme.txt
@@ -16,14 +16,14 @@ following anywhere in your page:
 
 [hungryfeed url="http://verysimple.com/feed/"]
 
-Features:
+= Features =
 
 * Uses WordPress Shortcodes for simple inclusion of feeds on any page or post
 * Has a variety of parameters to control the output and what RSS fields appear
 * Relies on WordPress built-in SimplePie for reading RSS
 * Fixes characters in URLs that may get mangled when editing in Visual mode
 * Caches feeds and allows configuration of the cache expiration
-* Outputs clean, stylable HTML and provides a configuration setting for adding style code
+* Outputs clean, HTML for easy styling with a CSS configuration setting
 
 == Installation ==
 
@@ -33,13 +33,13 @@ Features:
 
 == Screenshots ==
 
-1. HungryFEED hungry for RSS feeds!
-2. Enter a Shortcode on your page or post
-3. The RSS feed is pulled, cached and displayed inline
+1. Enter a Shortcode on your page or post
+2. The RSS feed is pulled, cached and displayed inline
+3. HungryFEED hungry for RSS feeds!
 
 == Frequently Asked Questions ==
 
-= What is HungryFEED =
+= What is HungryFEED? =
 
 HungryFEED is a plugin that includes an RSS feed within the content of a page or post
 
@@ -49,12 +49,19 @@ Documentation and support is available on the plugin homepage.
 
 == Upgrade Notice ==
 
-= 1.1.0 =
-* Improved error reporting when feeds cannot be read
-* Added awesome logo
+= 1.2.0 =
+Added two formatting option for enable/disable title link and formatting post date
 
 == Changelog ==
 
-= 1.0.0 =
-* Initial Release
+= 1.2.0 =
+* Added parameter item_link_title to enable/disable the post title link
+* Added parameter date_format to format the post date
 
+= 1.1.0 =
+* Improved feed reading when special characters get mangled by visual editor
+* Improved error reporting when feeds cannot be read
+* Awesome hungry monster logo added
+
+= 1.0.0 =
+*  Initial Release
